5cec0a497ea9913e082f4cf16c516c0e7d1dcdd4
Arjaz <arjaz@protonmail.com>
2019-01-28 15:59:22 +0200

    Minor fixes

--- blog/models.py
+++ blog/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255);
-    pub_date = models.DateTimeField();
-    body = models.TextField();
-    image = models.ImageField(upload_to='images/');
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
 
     def __str__(self):
         return self.title
